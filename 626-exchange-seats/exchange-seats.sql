de52348dfbf90d284107739446e3ad1213c186c6
Rohit Rathod <rohit1212rathod@gmail.com>
2026-02-03 21:55:20 +0530

    Time: 299 ms (96.15%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT
+    id,
+    CASE
+        WHEN id % 2 = 1 AND id < (SELECT MAX(id) FROM Seat)
+            THEN LEAD(student) OVER (ORDER BY id)
+        WHEN id % 2 = 0
+            THEN LAG(student) OVER (ORDER BY id)
+        ELSE student
+    END AS student
+FROM Seat;
