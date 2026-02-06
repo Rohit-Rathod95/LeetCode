91e37477c2ff577315eec38f7247cd164c186175
Rohit Rathod <rohit1212rathod@gmail.com>
2026-02-06 16:06:35 +0530

    Time: 6 ms (96.86%) | Memory: 45.4 MB (34.49%) - LeetSync

---
+++ 1351-replace-the-substring-for-balanced-string/replace-the-substring-for-balanced-string.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int balancedString(String s) {
+        int n = s.length();
+        int k = n / 4;
+
+        int[] count = new int[128];
+        for (char ch : s.toCharArray()) {
+            count[ch]++;
+        }
+        
+        if (count['Q'] == k && count['W'] == k &&
+            count['E'] == k && count['R'] == k) {
+            return 0;
+        }
+
+        int left = 0;
+        int ans = n;
+
+        for (int right = 0; right < n; right++) {
+            count[s.charAt(right)]--;
+
+            while (left < n &&
+                   count['Q'] <= k &&
+                   count['W'] <= k &&
+                   count['E'] <= k &&
+                   count['R'] <= k) {
+
+                ans = Math.min(ans, right - left + 1);
+                count[s.charAt(left)]++;
+                left++;
+            }
+        }
+
+        return ans;
+    }
+}
